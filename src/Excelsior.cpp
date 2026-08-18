ccab7a078d5b48cb3bc984536b282655ed9aaac8
Hemant Bhandari <zenithtaiga@gmail.com>
2026-08-18 13:51:14 +0530

    Add project files.

---
+++ src/Excelsior.cpp
@@ -0,0 +1,12 @@
+﻿// Excelsior.cpp : Defines the entry point for the application.
+//
+
+#include "Excelsior.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ src/Excelsior.h
@@ -0,0 +1,8 @@
+﻿// Excelsior.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
