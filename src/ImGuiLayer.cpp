399a6329af0f7621fcea1d71db640787f27372cf
Hemant Bhandari <zenithtaiga@gmail.com>
2026-09-04 05:01:55 +0530

    class constructor
    setup imgui context and initialize SDL3/OpenGL

--- src/ImGuiLayer.cpp
+++ src/ImGuiLayer.cpp
@@ -2,6 +2,8 @@
 
 #include "Window.hpp"
 
+#include <stdexcept>
+
 #include <glad/gl.h>
 
 #include <imgui.h>
@@ -9,5 +11,36 @@
 #include <imgui_impl_sdl3.h>
 
 namespace excelsior {
+	ImGuiLayer::ImGuiLayer(Window& window) : m_window(window) {
+		// setup imgui context
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
 
+		ImGuiIO& io = ImGui::GetIO();
+
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+		ImGui::StyleColorsDark();
+
+		ImGuiStyle& style = ImGui::GetStyle();
+
+		style.ScaleAllSizes(m_window.contentScale());
+		style.FontScaleDpi = m_window.contentScale();
+
+		// setup platform/renderer backends
+		if (!ImGui_ImplSDL3_InitForOpenGL(
+			m_window.nativeHandle(),
+			m_window.glContext()
+		)) {
+			ImGui::DestroyContext();
+			throw std::runtime_error("Couldn't initialize ImGui SDL3 backend");
+		}
+
+		if (!ImGui_ImplOpenGL3_Init("#version 430")) {
+			ImGui_ImplSDL3_Shutdown();
+			ImGui::DestroyContext();
+
+			throw std::runtime_error("Couldn't initialize ImGui OpenGL backend");
+		}
+	}
 }
