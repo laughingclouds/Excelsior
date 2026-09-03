dc73d436238df3ae569cd96e00d713306c109ae1
Hemant Bhandari <zenithtaiga@gmail.com>
2026-09-04 04:45:26 +0530

    Add imgui layer
    Code to initialize imgui stuff

---
+++ src/ImGuiLayer.cpp
@@ -0,0 +1,13 @@
+#include "ImGuiLayer.hpp"
+
+#include "Window.hpp"
+
+#include <glad/gl.h>
+
+#include <imgui.h>
+#include <imgui_impl_opengl3.h>
+#include <imgui_impl_sdl3.h>
+
+namespace excelsior {
+
+}
---
+++ src/ImGuiLayer.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <array>
+
+#include <SDL3/SDL_events.h>
+
+namespace excelsior {
+	class Window;
+
+	class ImGuiLayer {
+	public:
+		explicit ImGuiLayer(Window& window);
+		~ImGuiLayer();
+
+		ImGuiLayer(const ImGuiLayer&) = delete;
+		ImGuiLayer& operator=(const ImGuiLayer&) = delete;
+		ImGuiLayer(ImGuiLayer&&) = delete;
+		ImGuiLayer& operator=(ImGuiLayer&&) = delete;
+
+		void processEvent(const SDL_Event& event);
+		
+		void beginFrame();
+		
+		void render(const std::array<float, 4>& clearColor);
+
+	private:
+		Window& m_window;
+	};
+}
