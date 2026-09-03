1da13a794d326e312ef87abdf77885c7e82c6055
Hemant Bhandari <zenithtaiga@gmail.com>
2026-09-04 05:18:24 +0530

    Create PenInput class, PenState struct
    PenState will hold information about the pen
    PenInput deals with input and handles pen related events

---
+++ src/PenInput.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <SDL3/SDL_events.h>
+
+namespace excelsior {
+	struct PenState {
+		float x = -1.0f;
+		float y = -1.0f;
+
+		float rawPressure = 0.0f;
+		float pressure = 0.0f;
+
+		float tiltX = 0.0f;
+		float tiltY = 0.0f;
+	};
+
+	class PenInput {
+	public:
+		static constexpr float PRESSURE_OFFSET = 0.5f;
+
+		void processEvent(const SDL_Event& event);
+
+		[[nodiscard]]
+		const PenState& state() const noexcept;
+
+		[[nodiscard]]
+		bool isDrawing() const noexcept;
+
+	private:
+		void processMotion(const SDL_Event& event);
+		void processAxis(const SDL_Event& event);
+
+		PenState m_state;
+	};
+}
